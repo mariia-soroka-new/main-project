845f059658c5f2850f383e0cd5a8d95a62ea3dc6
mariia-soroka-new <masha.zefirova@gmail.com>
2020-05-16 19:49:57 +0000

    jquery UP button added

--- js/index.js
+++ js/index.js
@@ -2,3 +2,15 @@ const clock = document.querySelector('.clock');
 const showClock = () => clock.innerText = (new Date()).toLocaleTimeString();
 setInterval(showClock, 1000);
 
+var $btnTop = $(".btn-top")
+$(window).on("scroll", function(){
+    if ($(window).scrollTop() >= 10){
+        $btnTop.fadeIn();
+    }else{
+        $btnTop.fadeOut();
+    }
+});
+
+$btnTop.on( "click", function(){
+    $("html, body").animate({scrollTop:0}, 900)
+});
